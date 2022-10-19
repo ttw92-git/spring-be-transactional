9bf889b3d60e7a0b4f9940323d69f1abf6883ba2
TAN TA HWE <U064809@bca.co.id>
2022-10-19 17:00:27 +0700

    initial commit

---
+++ src/main/java/ttw/springbe/transactional/TransactionalApplication.java
@@ -0,0 +1,13 @@
+package ttw.springbe.transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionalApplication.class, args);
+	}
+
+}
